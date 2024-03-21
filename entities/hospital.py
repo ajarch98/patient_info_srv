1741b65f767de5bf27e04a4e36a87b9f3f51c172
AR487843 <AR487843@DAL.CA>
2024-03-21 18:34:45 -0300

    Added class Hospital and Patient Wrote test funtion in main.py to see if records printed correctly

--- entities/hospital.py
+++ entities/hospital.py
@@ -1,2 +1,19 @@
 class Hospital:
     name:str
+    address:str
+    city:str
+    state:str
+    zipcode:str
+    phone:str
+    email:str
+    website:str
+    
+    def __init__(self, name, address, city, state, zipcode, phone, email, website):
+        self.name = name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.phone = phone
+        self.email = email
+        self.website = website
--- entities/patient.py
+++ entities/patient.py
@@ -1,2 +1,23 @@
 class Patient:
-    name: str
+    def __init__(self, name: str, age: int, gender: str, address: str, phone: str, email: str,
+                 date_of_birth: str, nationality: str, marital_status: str, occupation: str,
+                 medical_history: str, allergies: str, medications: str, blood_type: str,
+                 insurance_info: str, primary_care_physician: str, emergency_contact: str, id: int):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.address = address
+        self.phone = phone
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.nationality = nationality
+        self.marital_status = marital_status
+        self.occupation = occupation
+        self.medical_history = medical_history
+        self.allergies = allergies
+        self.medications = medications
+        self.blood_type = blood_type
+        self.insurance_info = insurance_info
+        self.primary_care_physician = primary_care_physician
+        self.emergency_contact = emergency_contact
